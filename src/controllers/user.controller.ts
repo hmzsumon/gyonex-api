775d084af322f117214ad92eac42d57be2c35823
Zakaria Sumon <zsumonn@gmail.com>
2026-07-07 20:45:57 +0600

    update

--- src/controllers/user.controller.ts
+++ src/controllers/user.controller.ts
@@ -35,7 +35,7 @@ import jwt, { JwtPayload, Secret } from "jsonwebtoken";
 import mongoose, { ProjectionType, Types } from "mongoose";
 import { StringValue } from "ms";
 
-const default_referral_code = "UB46AD4532D";
+const default_referral_code = "UD71DFE67A0";
 
 /* ── 🔐 Register user ───────────────────────────────── */
 export const registerUser: typeHandler = catchAsync(async (req, res, next) => {
